c44225573f8d3ff9c777244fe7d6fd24767e6467
Gustavo Iago <gustavoiago@hotmail.com>
2024-05-29 10:25:50 +0200

    añadindo la logica del historial de contraseñas tambien al crear un nuevo usuario

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\HistorialContrasena;
 use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
@@ -12,7 +13,6 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Form\FormErrorIterator;
 
 #[IsGranted('ROLE_ADMIN')]
 class RegistrationController extends AbstractController
@@ -26,20 +26,26 @@ class RegistrationController extends AbstractController
 
         if ($form->isSubmitted()) {
             if ($form->isValid()) {
-                // encode the plain password
-                $user->setPassword(
-                    $userPasswordHasher->hashPassword(
-                        $user,
-                        $form->get('plainPassword')->getData()
-                    )
+                // Encode the plain password
+                $hashedPassword = $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
                 );
+                $user->setPassword($hashedPassword);
 
                 // Actualiza la propiedad isActive
                 $user->setIsActive(true); // Por defecto, el usuario estará activo al registrarse
                 $user->setActualizadoEn(new \DateTime("now", new \DateTimeZone('Europe/Madrid'))); // Asignar la fecha actual
                 $user->setCreadoEn(new \DateTime("now", new \DateTimeZone('Europe/Madrid'))); // Asignar la fecha actual
 
+                // Crear entrada en HistorialContrasena
+                $passwordHistory = new HistorialContrasena();
+                $passwordHistory->setHashedPassword($hashedPassword);
+                $passwordHistory->setUser($user);
+                $passwordHistory->setChangedAt(new \DateTime("now", new \DateTimeZone('Europe/Madrid')));
+
                 $entityManager->persist($user);
+                $entityManager->persist($passwordHistory);
                 $entityManager->flush();
 
                 return $this->redirectToRoute('app_login');
